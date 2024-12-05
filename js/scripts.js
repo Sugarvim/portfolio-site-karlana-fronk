b879063048406b6ba750b806925c07b9a8bf82df
Sugarvim <sugarvim@tutanota.com>
2024-12-05 12:06:26 -0500

    added linters

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 console.log("Confetti cake");
+// eslint-disable-next-line no-unused-vars
 function  menuToggle(){
     var x= document.getElementById("myNavtoggle");
     if (x.className === ("navtoggle")) {
     x.className += " responsive";
     }
     else {
-        x.className = "navtoggle"
+        x.className = "navtoggle";
     }
 }
