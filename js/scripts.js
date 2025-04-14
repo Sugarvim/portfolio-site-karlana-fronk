0c675b7ceaba9a3158e8785bfa7762ef24e6f27e
Sugarvim <sugarvim@tutanota.com>
2025-04-14 18:14:03 -0400

    Added-project-page
    Made separate page for all projects and Vela. Replaced dummy icons and changed email link. Added resume doc. Menu formatting fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 console.log("Confetti cake");
+// eslint-disable-next-line no-unused-vars
 function  menuToggle(){
     let x= document.getElementById("myNavtoggle");
     if (x.className === ("navtoggle")) {
