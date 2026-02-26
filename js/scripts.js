daa4207c8d57e6794c0374aeae23cff8ec1fe322
Sugarvim <sugarvim@tutanota.com>
2026-02-26 00:30:15 -0500

    user persona, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
+/* eslint-disable no-unused-vars */
 console.log("Confetti cake");
-// eslint-disable-next-line no-unused-vars
+
+const kip = document.querySelector("#Kip")
+const kipBtn = document.querySelector("#kip-btn")
+const lock = document.querySelector("#Lock")
+const lockBtn = document.querySelector("#lock-btn")
+const nav = document.querySelector("#myNavtoggle")
+
 function  menuToggle(){
-    let x= document.getElementById("myNavtoggle");
-    if (x.className === ("navtoggle")) {
-    x.className += " responsive";
+    if (nav.className === "navtoggle") {
+    nav.className += " responsive";
     }
     else {
-        x.className = "navtoggle";
+        nav.className = "navtoggle";
     }
 }
+
+kipBtn.addEventListener("click", () =>{
+    if(kipBtn.classList.contains("open")){
+        kipBtn.classList.remove("open")
+        kip.classList.remove("shrink")
+    }else{
+        kipBtn.classList.add("open")
+        kip.classList.add("shrink")
+    }
+})
+lockBtn.addEventListener("click", () =>{
+    if(lockBtn.classList.contains("open")){
+        lockBtn.classList.remove("open")
+        lock.classList.remove("shrink")
+    }else{
+        lockBtn.classList.add("open")
+        lock.classList.add("shrink")
+    }
+})
